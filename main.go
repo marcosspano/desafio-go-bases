5b6c7b5a6db637768e1232592d684338062b4e9e
marcosspano <e.marcosspano@gmail.com>
2022-11-16 03:04:40 -0300

    Importa github

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-)
 
-// "github.com/bootcamp-go/desafio-go-bases/internal/tickets"
+	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
+	// "github.com/marcosspano/desafio-go-bases/tree/main/internal/tickets"
+)
 
 func main() {
 
-	messageTotalTickets, _ := GetTotalTickets("China")
+	messageTotalTickets, _ := tickets.GetTotalTickets("China")
 	fmt.Println(messageTotalTickets)
 
-	messageTotalPeriodo, _ := GetCountByPeriod("tarde")
+	messageTotalPeriodo, _ := tickets.GetCountByPeriod("tarde")
 	fmt.Println(messageTotalPeriodo)
 }
